73174ca4b3d821f28123421bcacee2b403bcd917
Gabriel Cruz <62117580+gabrielzurc10@users.noreply.github.com>
2021-02-26 00:09:06 -0600

    Add files via upload

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -1,23 +1,32 @@
 
 
-var dice1 = document.querySelector(".img1");
-var dice2 = document.querySelector(".img2");
-var h1 = document.querySelector("h1");
+var dice1 = $(".img1");
+var dice2 = $(".img2");
+var h1 = $("h1");
 
-function rollDice() {
+$("button").click( function () {
   var randomNumber1 = Math.floor((Math.random() * 6) + 1);
   var randomNumber2 = Math.floor((Math.random() * 6) + 1);
   var randomDiceImage1 = "images/dice" + randomNumber1 + ".svg";
   var randomDiceImage2 = "images/dice" + randomNumber2 + ".svg";
 
-  dice1.setAttribute("src", randomDiceImage1);
-  dice2.setAttribute("src", randomDiceImage2);
+  var sound = new Audio("sounds/dice.mp3");
 
-  if (randomDiceImage1 > randomDiceImage2) {
-  h1.innerHTML = "Player 1 wins!";
-  } else if (randomDiceImage1 < randomDiceImage2) {
-  h1.innerHTML = "Player 2 wins!";
-  } else {
-  h1.innerHTML = "Draw!";
-  }
-}
+  $(".animation").fadeOut(function() {
+    dice1.attr("src", randomDiceImage1);
+    dice2.attr("src", randomDiceImage2);
+
+    if (randomDiceImage1 > randomDiceImage2) {
+    h1.html("Player 1 wins!");
+    } else if (randomDiceImage1 < randomDiceImage2) {
+    h1.html("Player 2 wins!");
+    } else {
+    h1.html("Draw");
+    }
+
+    sound.play();
+  });
+
+  $(".animation").fadeIn();
+
+});
